3c5ed83011209d882ab902dbca3eac1ba7031726
selimekim94 <selimekim94@gmail.com>
2020-01-13 15:15:59 +0300

    users app creation finished.

--- users/models.py
+++ users/models.py
@@ -4,4 +4,37 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    bio = models.TextField(default='')
+    """ Custom User Model """
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
+
+    LANGUAGE_EN = 'en'
+    LANGUAGE_TR = 'tr'
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_EN, 'English'),
+        (LANGUAGE_TR, 'Turkish')
+    )
+
+    CURRENCY_USD = 'usd'
+    CURRENCY_TRY = 'try'
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, 'USD'),
+        (CURRENCY_TRY, 'TRY')
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    super_host = models.BooleanField(default=False)
