25eb46a8e372c5838967bebf77e8beaa82a825c6
selimekim94 <selimekim94@gmail.com>
2020-01-15 14:47:41 +0300

    lists app creation finished.

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import TimeStampedModel
+from users.models import User
+from rooms.models import Room
 
-# Create your models here.
+
+class List(TimeStampedModel):
+    """ List Model Definition """
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(Room, blank=True)
+
+    def __str__(self):
+        return self.name
